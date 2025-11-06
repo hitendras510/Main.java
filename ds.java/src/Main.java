be85e00f18107882ff191f1d8d1a070bc2911d3e
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-06 13:13:14 +0530

    new

--- ds.java/src/Main.java
+++ ds.java/src/Main.java
@@ -1,4 +1,4 @@
-////import java.util.Scanner;
+import java.util.Scanner;
 ////
 ////public class Main {
 ////   public static void main(String[] args) {
@@ -46,17 +46,67 @@
 //}
 //
 //
-class Main{
-    public static void main(String[] args){
-        String str="Hello";
-        System.out.println("length of string:"+ str.length());
-        System.out.println("char of string at defined index:"+ str.charAt(1));
-        System.out.println("char to uppercase:"+ str.toUpperCase());
-        System.out.println("char to lower case:"+ str.toLowerCase());
-        System.out.println(":"+ str.trim());
-        System.out.println("starting point with given index no.:"+ str.substring(2));
-        System.out.println("start and end point as need :"+ str.substring(1 , 4));
-        System.out.println("index number:"+ str.indexOf(str));
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word:");
+        String word = sc.nextLine();
+        String b = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            b += word.charAt(i);
+        }
+
+        if (word.equals(b)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+
+
+
+
+
+
+//        String str="Hello";
+//        String str1="Hello;
+//String str=new String("Hello");
+//String str1=new String("Hello");
+//
+//System.out.println(str==str1);
+//System.out.println(str.equals(str1));
+
+
+//        System.out.println("length of string:"+ str.length());
+//        System.out.println("char of string at defined index:"+ str.charAt(1));
+//        System.out.println("char to uppercase:"+ str.toUpperCase());
+//        System.out.println("char to lower case:"+ str.toLowerCase());
+//        System.out.println(":"+ str.trim());
+//        System.out.println("starting point with given index no.:"+ str.substring(2));
+//        System.out.println("start and end point as need :"+ str.substring(1 , 4));
+//        System.out.println("index number:"+ str.indexOf(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
