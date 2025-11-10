833216d85aa3b16968d485c1994d7d2b14bc2578
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-10 13:56:47 +0530

    new

--- ds.java/Lectures/Main.java
+++ ds.java/Lectures/Main.java
@@ -143,22 +143,25 @@ class Main {
 
 
 
-String str = "aaaaabbbccd";
-StringBuilder result = new StringBuilder();
-int count = 1;
-
-for(int i=1;i<=str.length();i++){
-    if(str.charAt(i)==str.charAt(i-1)){
-        count++;
-    }else {
-        result.append(str.charAt(i-1));
-    }
-}
-result.append(count);
+        String str = "aaaaabbbccd";
+        StringBuilder result = new StringBuilder();
+        int count = 1;
 
 
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
 
+                result.append(str.charAt(i - 1)).append(count);
 
+                count = 1;
+            }
+        }
+
+        result.append(str.charAt(str.length() - 1)).append(count);
+
+        System.out.println(result);
 
 
 
